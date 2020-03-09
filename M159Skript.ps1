17c45cdd071f973f4a3aa2d6c74a7ea5479ae170
SwissPrekek <fabian.zeller@gibmit.ch>
2020-03-09 22:19:58 +0100

    Zuteilung Organisationseinheit und Gruppen funktioniert.
    Export in ein CSV oder auch LDIF funktioniert nicht.
    Ich kriege die Daten auch nicht schön in eine Reihe formatiert

--- M159Skript.ps1
+++ M159Skript.ps1
@@ -16,43 +16,100 @@ function get-sanitizedUTF8Input {
 #Functions End
 
 #Definition of all Variables
-$tables = Import-Csv -Path "J:\1_Bibliothek\School\Modul159\ADmini.csv" -Delimiter ";"
+$tables = Import-Csv -Path "J:\1_Bibliothek\School\Modul159\ADMini.csv" -Delimiter ";"
 
 $user = @()
+$uservorname = @()
+$usernachname = @()
 $userGroup = @()
 $userOu = @()
 $username = @()
-$organizationalunits = @{"O_Gertzenstein" = "O_Gertzenstein";"O_Deaktiviert" = "O_Deaktiviert";"O_Diverse" = "O_Diverse";"O_Lehrer" = "O_Lehrer";"O_Schueler" = "O_Schueler";"O_Oberstufe" = "O_Oberstufe";"O_Verwaltung" = "O_Verwaltung"}
-$groups = @{"G_Deaktiviert" = "507";"G_Diverse" = "508";"GL_Gymnasium" = "505";"GL_Handelsmatura" = "506";"GL_Sekundarschule" = "504";"GS_Handelsmatura" = "510";"GS_Matura" = "511";"G_Verwaltung" = "509"}
+#$organizationalunits = @{"O_Gertzenstein" = "O_Gertzenstein"; "O_Deaktiviert" = "O_Deaktiviert"; "O_Diverse" = "O_Diverse"; "O_Lehrer" = "O_Lehrer"; "O_Schueler" = "O_Schueler"; "O_Oberstufe" = "O_Oberstufe"; "O_Verwaltung" = "O_Verwaltung" }
+$groups = @{"G_Deaktiviert" = "507"; "G_Diverse" = "508"; "GL_Gymnasium" = "505"; "GL_Handelsmatura" = "506"; "GL_Sekundarschule" = "504"; "GS_Sekundarschule" = "504"; "GS_Handelsmatura" = "510"; "GS_Matura" = "511"; "G_Verwaltung" = "509" }
 #End Of Definition of all Variables
 
 #Username generation
-foreach($x in $tables){
-  $vorname = get-sanitizedUTF8Input $x.vorname.ToLower()
-  $nachname = get-sanitizedUTF8Input $x.nachname.ToLower()
-  $username += $vorname +"."+ $nachname
+foreach ($x in $tables) {
+    $vorname = get-sanitizedUTF8Input $x.vorname.ToLower()
+    $nachname = get-sanitizedUTF8Input $x.nachname.ToLower()
+    $username += $vorname + "." + $nachname
+    $uservorname += $vorname
+    $usernachname += $nachname
 
 }
 
-#OrganizationUnit generation
-foreach($x in $tables){
-    if ($x.Beschreibung -match "Sekundar" -And !($x.Beschreibung -match "Lehrer")) {
-    $userOu = "O_Schueler"
-}
- else{
- $userOu = "empty"
- } 
+#OrganizationUnit and Group generation
+foreach ($x in $tables) {
+    if (!($x.Beschreibung -match "Lehrer") -and !($x.Beschreibung -match "Verwaltung") -and !($x.Beschreibung -match "deaktiviert")) {
+        $userOu += "O_Schueler"
+        if ($x.Beschreibung -match "Sekundar") {
+            $userGroup += $groups.GS_Sekundarschule
+        }
 
+        elseif ($x.Beschreibung -match "Handelsmatur") {
+            $userGroup += $groups.GS_Handelsmatura
+        }
+
+        elseif ($x.Beschreibung -match "Matur") {
+            $userGroup += $groups.GS_Matura
+        }
+
+    }
+
+    elseif ($x.Beschreibung -match "Lehrer") {
+        $userOu += "O_Lehrer"
+
+        if ($x.Beschreibung -match "Sekundar") {
+            $userGroup += $groups.GL_Sekundarschule
+        }
+        elseif ($x.Beschreibung -match "Gym") {
+            $userGroup += $groups.GL_Gymnasium
+        }
+        elseif ($x.Beschreibung -match "Handel") {
+            $userGroup += $groups.GL_Handelsmatura
+        }
+
+    }
+
+    elseif ($x.Beschreibung -match "Verwaltung") {
+        $userOu += "O_Verwaltung"
+        $userGroup += $groups.G_Verwaltung
+        
+    }
+    elseif ($x.Beschreibung -match "deaktiviert") {
+        $userOu += "O_Deaktiviert"
+        $userGroup += $groups.G_Deaktiviert
+    }
+    else {
+        $userOu = "empty"
+    } 
 }
 
 # Object for a user with the necessary properties of this User
 $userobject = new-object PSObject -Property @{
+    vorname  = $uservorname;
+    nachname = $usernachname;
     username = $username;
-    ou = $userOu;
-    group = $userGroup;
+    ou       = $userOu;
+    group    = $userGroup;
        
-    }
+}
+$user += New-Object PSObject -Property $userobject
+$user | Export-Csv -Path J:\1_Bibliothek\School\Modul159\Git\CSVtoLDIF\ehren.csv -Delimiter ';' 
 
+<#
+foreach ($x in $userobject.username ){
+Write-Host "Benutzername: " $x
 
-$user += $userobject
-$user.ou
+}
+
+foreach ($x in $userobject.ou ){
+Write-Host "Organisationseinheit: " $x
+
+}
+foreach ($x in $userobject.group ){
+Write-Host "Gruppe: " $x
+
+}
+#>
+
